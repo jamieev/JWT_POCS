db2a5ebdfd5347e4032b517661dfd711ab1c6f01
Jamie Evans <jamieev@hotmail.com>
2016-11-25 15:16:13 +0000

    switched from passport to node-oauth2-server

--- test/app.test.js
+++ test/app.test.js
@@ -13,23 +13,26 @@ describe('Simple unsecured token endpoint', function() {
     myapp.stop(done);
   });
 
-  describe('when requested at /token with client_id and client_secret', function() {
+  describe('when requested at /oauth/token with client_id and client_secret', function() {
     it('should return a token', function(done) {
-      request.post(baseUrl + '/token').query({client_id:'client1'}).query({client_secret: 'secret'}).end(function assert(err, res) {
+      request.post(baseUrl + '/oauth/token').set('Content-Type', 'application/x-www-form-urlencoded')
+            .send({grant_type:'client_credentials',client_id:'thom',client_secret: 'nightworld'})
+            .end(function assert(err, res) {
         expect(err).to.not.be.ok;
         expect(res).to.have.property('status', 200);
         expect(res.text).to.be.a('string');
-        expect(res.text).to.contain('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhY2NvdW50c');
+        expect(res.text).to.contain('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ0aGl');
         done();
       });
     });
   });
 
-  describe('when requested at /token without client_id and client_secret', function() {
+  describe('when requested at /oauth/token without client_id and client_secret', function() {
     it('should return 400', function(done) {
-      request.post(baseUrl + '/token').end(function assert(err, res) {
+      request.post(baseUrl + '/oauth/token').set('Content-Type', 'application/x-www-form-urlencoded')
+          .send({grant_type:'client_credentials'}).end(function assert(err, res) {
          expect(res).to.have.property('status', 400);
-         expect(res.text).to.equal('include both client_id and client_secret in query string');
+         expect(res.text).to.contain('Invalid or missing client_id parameter');
         done();
       });
     });
@@ -38,10 +41,9 @@ describe('Simple unsecured token endpoint', function() {
   describe('when requested at / without a valid auth token', function() {
     it('should return unauthorized', function(done) {
       request.get(baseUrl + '/').end(function assert(err, res) {
-        expect(res).to.have.property('status', 401);
+        expect(res).to.have.property('status', 400);
         done();
       });
     });
   });
-
 });
